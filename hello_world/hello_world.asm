8fcbb1a1485afe81048ee42f0d6e98039cfe30e4
Nyfeu <andre.maiolini@hotmail.com>
2024-07-09 15:54:36 -0300

    Comentando print_rax.asm

--- hello_world/hello_world.asm
+++ hello_world/hello_world.asm
@@ -8,7 +8,7 @@
 
 
 	;  O rótulo que demarca a primeira instrução deve ser declarado como: 'global'.
-  ;  Isso faz com que o ponto de entrada seja identificado e, posteriormente, acessível ao linker.
+        ;  Isso faz com que o ponto de entrada seja identificado e, posteriormente, acessível ao linker.
 
 
 	global _start
@@ -18,16 +18,16 @@
 
 
 	;  Essa seção é responsável pelas variáveis globais (disponíveis a qualquer momento da execução 
-  ;  do programa).
+        ;  do programa).
 
 
 	section .data
 	message: db 'Hello, World!', 10   ;  Mensagem a ser escrita - através da diretiva 'db' (data byte) -
-                                    ;  com nova linha ao final. O byte '10' é o caractere especial de 
-                                    ;  nova linha na tabela ASCII - '\n' (End of Line).  
-                                    ;  A separação por vírgula garante que o caractere especial
-                                    ;  será alocado no endereço seguinte ao da cadeia de caracteres,
-                                    ;  facilitando o acesso através do buffer 'message'.
+                                          ;  com nova linha ao final. O byte '10' é o caractere especial de 
+                                          ;  nova linha na tabela ASCII - '\n' (End of Line).  
+                                          ;  A separação por vírgula garante que o caractere especial
+                                          ;  será alocado no endereço seguinte ao da cadeia de caracteres,
+                                          ;  facilitando o acesso através do buffer 'message'.
 
 ;  **********  Seção - text  ******************************i******************************************  ;
 
@@ -37,20 +37,20 @@
 
 	section .text
 	_start:                           ;  Para nos livrarmos dos endereços numéricos, usamos rótulos.
-                                    ;  A instrução 'mov' serve para atribuir um valor ao registrador
-                                    ;  ou para um endereço na memória.
+                                          ;  A instrução 'mov' serve para atribuir um valor ao registrador
+                                          ;  ou para um endereço na memória.
 
-	  	mov  rax, 1                   ;  O número da chamada de sistema (write)
-		  mov  rdi, 1                   ;  Argumento (#1) em rdi: onde escrever -> stdout
-		  mov  rsi, message             ;  Argumento (#2) em rsi: onde começa a string (buffer)
-		  mov  rdx, 14                  ;  Argumento (#3) em rdx: quantos bytes devem ser escritos
-		  syscall                       ;  Executa a chamada de sistema
+	  	mov  rax, 1               ;  O número da chamada de sistema (write)
+		  mov  rdi, 1             ;  Argumento (#1) em rdi: onde escrever -> stdout
+		  mov  rsi, message       ;  Argumento (#2) em rsi: onde começa a string (buffer)
+		  mov  rdx, 14            ;  Argumento (#3) em rdx: quantos bytes devem ser escritos
+		  syscall                 ;  Executa a chamada de sistema
 
 
-		  mov  rax, 60                  ;  O número da chamada de sistema (exit).
-		  xor  rdi, rdi                 ;  Zera o registrador rdi - que será o responsável
-		  syscall                       ;  pelo código de retorno do programa.
-                                    ;  Executa a chamada de sistema de forma equivalente à 
-                                    ;  'return 0' da linguagem C.
+		  mov  rax, 60            ;  O número da chamada de sistema (exit).
+		  xor  rdi, rdi           ;  Zera o registrador rdi - que será o responsável
+		  syscall                 ;  pelo código de retorno do programa.
+                                          ;  Executa a chamada de sistema de forma equivalente à 
+                                          ;  'return 0' da linguagem C.
 
 ;  **************************************************************************************************  ;
--- print_reg/print_rax.asm
+++ print_reg/print_rax.asm
@@ -2,65 +2,104 @@ global _start
 
 section .data
 
-	codes: db '0123456789ABCDEF'  ; Através do deslocamento do
-                                ; endereço codes, pode-se acessar
-                                ; todos os símbolos hexadecimais
-
-  EOL: db 10                    ; Caractere end_of_line 
+	codes: db '0123456789ABCDEF'  	    ; Caracteres ASCII utilizados para representação
+                                            ; dos números em formato hexadecimal.
+                                
+  	EOL: db 10                          ; Caractere especial '\n' (End of Line).
 	
 section .text
 
 	_start:
 	
-		mov rax, 0x1122334455667788  ; número em formato hexa
+		mov rax, 0x1122334455667788 ; Número em formato hexadecimal.
 		
-		mov rdi, 1
-		mov rdx, 1
-    mov rcx, 64
+		; Configurando argumentos para a syscall: write
 		
-		; cada 4 bits devem ser exibidos como um dígido hexadecimal
+		mov rdi, 1                  ; Define o descritor de arquivo: 'stdout'
+		mov rdx, 1                  ; Número de bytes a serem escritos
+		
+		; Criando um contador de bits para o registrador:
+		
+		mov rcx, 64                 ; Quantidade total de bits em 'rax'.
+		
+		; Cada 4 bits devem ser exibidos como um dígido hexadecimal,
+		; para isso, será utilizado um loop até o último caractere
+		; ter sido escrito em 'stdout'.
 		
 	.loop:
-
-		push rax
-		
-		sub rcx, 4
-		sar rax, cl
-		and rax, 0xf
-		
-		lea rsi, [codes + rax]      ; Cria um deslocamento sobre codes
-		mov rax, 1
-		
-		push rcx
-		syscall
-		
-		pop rcx
-		pop rax
 	
-    ; Enquanto rcx não for zero, continua a execução do loop.
-    ; Caso seja zero, a Zero Flag será definida e 
-    ; o salto condicional não será executado. 
+		; Iniciando o ciclo de repetição que irá escrever cada dígito hexa.
 
-		test rcx, rcx
-		jnz .loop
+		push rax                    ; Guardando o valor de rax na stack
+		
+    		sub rcx, 4                  ; Subtraí 4 de rcx - ou seja: um digito hexadecimal,
+					    ; atuando como controle para o ciclo de repetição!
 
+		shr rax, cl                 ; cl é a menor parte de rcx (8 bits) - LSBs - sendo
+		                            ; suficiente para representação de um 'char'.
+		
+    		; A instrução 'shr' (shift right) desloca os bits para a direita. Usar 'sar'
+    		; (shift arithmetic right) teria o mesmo efeito - neste caso - pois não estamos 
+    		; lidando com números sinalizados.
+
+		and rax, 0xf                ; Aplica o operador AND (bitwise) com 0b1111
+		
+		; Isso resulta na aplicação de uma máscara para os 4 LSBs de rax.
+		; Ou seja, isola o caractere anteriormente deslocado pelo 'shr'.
+		
+		lea rsi, [codes + rax]      ; Load Effective Address (lea), carrega o caractere
+		                            ; referenciado por [codes + rax] em rsi.
+		                            ; Por exemplo: [codes + 10] => rsi = 'A'.
+		
+		; A próxima etapa é imprimir (escrever) o caractere, usando a syscall write.
+		
+		mov rax, 1                  ; Atribui o código da syscall write em rax
+		
+		push rcx                    ; Guarda o valor de rcx antes da chamada, pois,
+					    ; rcx será sobrescrito pela chamada.
+		
+		syscall                     ; Os argumentos já foram inicializados anteriormente,
+		                            ; então, é escrito o dígito hexa na 'stdout'.
+		
+		; Agora, deve-se recuperar os valores para que seja iterado o próximo caractere
+		
+		pop rcx                     ; Recupera o valor da contagem da stack
+		pop rax                     ; Recupera o número hexa original - sem nenhuma
+		                            ; operação aplicada, tal como: shifts e máscaras.
+		
+		; A instrução 'test' irá aplicar o operador AND (bitwise) entre rcx e ele mesmo,
+		; dessa forma, resultando no próprio valor de rcx. No entanto, essa instrução
+		; atualiza a flag de status ZF (Zero Flag) - ou seja: quando rcx for zero,
+		; irá ativar a flag e o salto condicional não será executado - finalizando o ciclo.
+		
+		test rcx, rcx               ; Testa se rcx é zero.
+		jnz .loop                   ; Caso não seja, retorna para o ínicio do loop.
+	
+	print_newline:
+	
+		; Para a escrita apropriada, será adicionado o caractere
+                ; especial: '\n' (end of line) ao final da cadeia desejada. 
+
+		; Estaremos aproveitando os argumentos já inicializados para 
+		; a chamada de sistema write - em rdi e rdx.
+
+    		mov rax, 1
+    		mov rsi, EOL
+    		syscall 
+		
 	_end:
-
-    ; Para a escrita apropriada, será adicionado o caractere
-    ; especial: '\n' (end of line). 
-
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, EOL
-    mov rdx, 1
-    syscall
-
-    ; xor irá verificar se todos os bits contidos em rdi são
-    ; iguais. Caso sejam, resultará em zero - ou seja, programa 
-    ; finalizado com sucesso. 
-
-    ; Esse resultado independerá do valor contido em rdi! 
-
+	
+		; Neste trecho é executada a syscall: exit - para finalizar o programa.
+	
 		mov rax, 60
 		xor rdi, rdi
+		
+		; xor irá verificar se todos os bits contidos nos operandos são
+    		; iguais. Caso sejam, resultará em zero - ou seja, programa finalizado com sucesso.
+		
+		; Como os operandos são ambos: rdi, independentemente do valor em rdi,
+		; carregará de uma forma simples o valor zero (0) em rdi - exit_code = 0.
+		
 		syscall
+		
+		; Programa finalizado com sucesso.
