ae86a652e61ce236c51e9b1943ffc5f66bbf679b
Nyfeu <andre.maiolini@hotmail.com>
2024-07-14 15:52:44 -0300

    Função: print_uint (unsigned integer 8 bytes)

--- io_library/io_library.asm
+++ io_library/io_library.asm
@@ -3,23 +3,28 @@ global _start
 section .data
 
   string_demo: db "Hello, World!", 0
-  new_line: db 10
+
+  new_line: db 10 
 
 section .text
 
-  _start:
+  _start:                         ; Código executado para testes
     
     mov rdi, string_demo
     call print_string
     call print_newline
 
-    mov rdi, string_demo
+    mov rdi, 0xffffffffffffffff   ; Carrega com o valor máximo de uint
+    call print_uint
+    call print_newline
+
+    mov rdi, string_demo          ; Conta os caracteres de string_demo
     call string_length
 
-    mov rdi, rax 
+    mov rdi, rax                  ; Passa o resultado como exit_code
     call exit
   
-  string_length:
+  string_length:                  ; Aceita arg: buffer string (rdi)
     
     xor rax, rax
 
@@ -43,8 +48,8 @@ section .text
     syscall
     ret
 
-  print_newline:
-    
+  print_newline:                  ; Escreve o char: '\n'
+                                  ; Não aceita argumentos
     mov rax, 1
     mov rdi, 1
     mov rsi, new_line
@@ -77,6 +82,65 @@ section .text
       jmp .iterate                ; e começa a próxima iteração
 
     .end:
+
+      ret
+
+  print_uint:                     ; Recebe arg: rdi (inteiro 64 bits)
+
+    mov rax, rdi                  ; Guarda arg em rax
+
+    ; Salva os valores dos registradores
+
+    push rbx
+    push rcx
+    push rdx
+    push rsi
+    
+    ; Alocando espaço na pilha (STACK)
+
+    sub rsp, 21                   ; Aloca 21 bytes na pilha (20 digitos + '\0')
+                                  ; MAX_UINT = 18.446.744.073.709.551.615
+    
+    ; Inicializar o buffer e índice
+
+    lea rbx, [rsp + 20]           ; rbx aponta para o final do buffer
+    mov byte[rbx], 0              ; Adiciona '\0' (nulo) ao final da cadeia
+
+    .convert:
+      
+      xor rdx, rdx                ; Zera os MSBytes do operando
+                                  ; Divide rdx:rax por s (div s)
+      
+      mov rcx, 10                 ; Define o divisor rcx = 10
+      div rcx                     ; (Q) rax = rax / 10, (R) rdx = rax % 10
+        
+      add dl, '0'                 ; Soma aos LSBs de rdx '0',
+                                  ; ou seja, converte para char (ASCII)
+        
+      dec rbx                     ; Move o ponteiro do buffer para a esquerda
+
+      mov byte[rbx], dl           ; Armazena o dígito no buffer
+        
+      test rax, rax               ; Verifica se rax é zero
+      jnz .convert                
+      
+    .print:
+      
+      ; Configurando os parâmetros da syscall write
+
+      lea rdi, [rbx]
+      call print_string
+
+      ; Desalocando o buffer da pilha (STACK)
+
+      add rsp, 21
+      
+      ; Recuperando valores iniciais dos registradores
+
+      pop rbx
+      pop rcx
+      pop rdx
+      pop rsi
 
       ret
 
